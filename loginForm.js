676d7b604b706f767d7a7754bb553de97774aec3
rishiD83 <rishi.singh@dianapps.com>
2021-07-30 09:11:09 +0530

    All Validation Added

--- loginForm.js
+++ loginForm.js
@@ -3,18 +3,35 @@ const email = document.getElementById("email");
 const phone = document.getElementById("phone");
 const website = document.getElementById("website");
 const textArea = document.getElementById("text");
-const form = document.getElementById("form")
+const form = document.getElementById("form");
 const submit = document.querySelector("btn");
 
-form.addEventListener("submit", (e)=> {
-    e.preventDefault();
-    if (name.value === "") {
-        alert("Please Enter a valid Name");
-    }
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (name.value === "" || name.value === null) {
+    alert("Please Enter a valid Name");
+    return false;
+  }
 
-    if (email.value ==="" ) {
-        alert("Please enter an email address ")
-        
-    }
+  if (email.value === "") {
+    alert("Please enter an email address ");
+    return false;
+  }
 
-}) 
+  if (phone.value === "") {
+    alert("Please enter a Mobile Number ");
+    return false;
+  }
+
+  if (email === "") {
+    alert("Please enter Website");
+    return false;
+  }
+
+  if (textArea.value === "") {
+    alert("Please enter your query");
+    return false;
+  }
+
+  else { alert("User Registered")}
+});
